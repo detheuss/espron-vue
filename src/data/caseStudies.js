a8a718754ef67512dae234446b33bda46d9fda98
Tomáš Richnavský <richnavsky.t@gmail.com>
2022-04-17 20:40:03 +0200

    updated case studies

--- src/data/caseStudies.js
+++ src/data/caseStudies.js
@@ -9,25 +9,120 @@ export default [
       {
         // src medium (1280 x 960)
         src: require("../assets/images/projects/family-house/familyHouse1-1280x960.jpeg"),
-        // src small (680 x 480)
-        thumbnail:require("../assets/images/projects/family-house/familyHouse1-680x480.jpeg"),
-        w: 1280,
-        h: 960,
-        alt: "Family House 1", // optional alt attribute for thumbnail image
-        title: "Family House"
-      },
-      {
-        // src medium (1280 x 960)
-        src: require("../assets/images/projects/family-house/familyHouse1-1280x960.jpeg"),
-        // src small (680 x 480)
-        thumbnail:require("../assets/images/projects/family-house/familyHouse1-680x480.jpeg"),
+        // src small (160 x 120)
+        thumbnail:require("../assets/images/projects/family-house/familyHouse1-160x120.jpeg"),
         w: 1280,
         h: 960,
         alt: "Family House 1", // optional alt attribute for thumbnail image
         title: "Family House"
       },
       // img 2
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/family-house/familyHouse2-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/family-house/familyHouse2-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Family House 2", // optional alt attribute for thumbnail image
+        title: "Family House"
+      },
+      // img 3
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/family-house/familyHouse3-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/family-house/familyHouse3-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Family House 3", // optional alt attribute for thumbnail image
+        title: "Family House"
+      },
     ],
     text: "Inspired by the aforementioned, we removed the wall between the kitchen and the living room to make it more airy and bright, as the previous outdated composition did not meet the criteria of current housing trends.",
+  },
+  {
+    id: "2",
+    title: "Great room project",
+    thumbImgSrc: require("../assets/images/projects/great-room/greatRoom1-640x480.jpeg"),
+    quote: "Nothing is better than going home to family and eating good food and relaxing.",
+    gallery: [
+      // img 1
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/great-room/greatRoom1-1280x960.jpeg"),
+        // src small (160 x 120)
+        thumbnail:require("../assets/images/projects/great-room/greatRoom1-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Great room 1", // optional alt attribute for thumbnail image
+        title: "Great room"
+      },
+      // img 2
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/great-room/greatRoom2-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/great-room/greatRoom2-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Great room 2", // optional alt attribute for thumbnail image
+        title: "Great room"
+      },
+      // img 3
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/great-room/greatRoom3-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/great-room/greatRoom3-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Great room 3", // optional alt attribute for thumbnail image
+        title: "Great room"
+      },
+    ],
+    text: "We create open space to bring families closer together. Our vast design allows family members to spend more time together in a comfortable setting. The great room combined with a modern American-style kitchen and an open living room creates a perfect balance of spacious and cozy elements.",
+  },
+  {
+    id: "3",
+    title: "Kitchen & bathroom project",
+    thumbImgSrc: require("../assets/images/projects/kitchen-bathroom/kitchenBathroom1-640x480.jpeg"),
+    quote: "Color is the place where our brains and universe meet.",
+    gallery: [
+      // img 1
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/kitchen-bathroom/kitchenBathroom1-1280x960.jpeg"),
+        // src small (160 x 120)
+        thumbnail:require("../assets/images/projects/kitchen-bathroom/kitchenBathroom1-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Kitchen & bathroom 1", // optional alt attribute for thumbnail image
+        title: "Kitchen & bathroom"
+      },
+      // img 2
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/kitchen-bathroom/kitchenBathroom2-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/kitchen-bathroom/kitchenBathroom2-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Kitchen & bathroom 2", // optional alt attribute for thumbnail image
+        title: "Kitchen & bathroom"
+      },
+      // img 3
+      {
+        // src medium (1280 x 960)
+        src: require("../assets/images/projects/kitchen-bathroom/kitchenBathroom3-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/kitchen-bathroom/kitchenBathroom3-160x120.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Kitchen & bathroom 3", // optional alt attribute for thumbnail image
+        title: "Kitchen & bathroom"
+      },
+    ],
+    text: "We enhanced this smaller space with a combination of wood and Egyptian blue kitchen unit to bring more glow and dynamic. The bathroom was redesigned in a similar manner, using contrasting mixture of black and white tiles.",
   },
 ];
