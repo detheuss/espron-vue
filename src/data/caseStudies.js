315aae0013d85aaa648e3639b9b10049b7382f27
Radovan Krizalkovic <radovan.krizalkovic@mikonboard.com>
2022-04-17 19:00:11 +0200

    require images

--- src/data/caseStudies.js
+++ src/data/caseStudies.js
@@ -2,31 +2,31 @@ export default [
   {
     id: "1",
     title: "Project Family House",
-    thumbImgSrc: "https://panoptes-uploads.zooniverse.org/project_attached_image/ddd67d1f-2371-4f8e-be2a-1b668a1e20f9.jpeg",
+    thumbImgSrc: require("../assets/images/projects/family-house/familyHouse3-680x480.jpeg"),
     quote: "An old thing becomes new if you detach it from what usually surrounds it.",
     gallery: [
       // img 1
       {
         // src medium (1280 x 960)
-        src: "https://panoptes-uploads.zooniverse.org/project_attached_image/152335a7-3e02-4814-b29d-c1c46bad36d2.jpeg",
-        // src small (640 x 345)
-        thumbnail:
-          "https://panoptes-uploads.zooniverse.org/project_attached_image/3848f0ff-4b11-4a4b-af88-46b720b55ab7.jpeg",
+        src: require("../assets/images/projects/family-house/familyHouse1-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/family-house/familyHouse1-680x480.jpeg"),
         w: 1280,
         h: 960,
         alt: "Family House 1", // optional alt attribute for thumbnail image
+        title: "Family House"
       },
-      // img 2
       {
         // src medium (1280 x 960)
-        src: "https://panoptes-uploads.zooniverse.org/project_attached_image/c7607999-6d6b-403e-9294-14402ec14e65.jpeg",
-        // src small (640 x 345)
-        thumbnail:
-          "https://panoptes-uploads.zooniverse.org/project_attached_image/31731b76-c1eb-4ee9-910f-5bd0998f53f5.jpeg",
-        w: 600,
-        h: 400,
-        alt: "Family House 2", // optional alt attribute for thumbnail image
+        src: require("../assets/images/projects/family-house/familyHouse1-1280x960.jpeg"),
+        // src small (680 x 480)
+        thumbnail:require("../assets/images/projects/family-house/familyHouse1-680x480.jpeg"),
+        w: 1280,
+        h: 960,
+        alt: "Family House 1", // optional alt attribute for thumbnail image
+        title: "Family House"
       },
+      // img 2
     ],
     text: "Inspired by the aforementioned, we removed the wall between the kitchen and the living room to make it more airy and bright, as the previous outdated composition did not meet the criteria of current housing trends.",
   },
