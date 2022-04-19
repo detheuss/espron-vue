5f811f6c0d08690f4b4994acd356db33ba742e6c
Radovan Krizalkovic <radovan.krizalkovic@mikonboard.com>
2022-04-19 21:37:27 +0200

    deployment script fix 2

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f 
-git@github.com:detheuss/espron-vue.git master:gh-pages
+git push -f git@github.com:detheuss/espron-vue.git master:gh-pages
 
 cd -
